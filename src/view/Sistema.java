2767d37e1d4cf51262304444993709ce6fe25074
Juan Díaz <juannavegante2010@gmail.com>
2024-02-08 23:38:56 -0500

    Se añade enrutador

--- src/view/Sistema.java
+++ src/view/Sistema.java
@@ -4,12 +4,10 @@
  */
 package view;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Image;
 import java.util.ArrayList;
-import javax.swing.Icon;
-import javax.swing.ImageIcon;
-import javax.swing.JLabel;
+import javax.swing.JPanel;
 import model.UserData;
 
 /**
@@ -21,8 +19,22 @@ public class Sistema extends javax.swing.JFrame {
 
     public Sistema() {
         initComponents();
+        
+        InitialSettings();
+
+    }
+    public void InitialSettings(){
+        //Centrar pantalla
         this.setLocationRelativeTo(null);
+        //Area de reescalado de imagenes del panel principal
+  
         rsscalelabel.RSScaleLabel.setScaleLabel(this.txtLogo, "src/img/logo4.png");
+        rsscalelabel.RSScaleLabel.setScaleLabel(this.iconVentas, "src/img/icons/ventas.png");
+        
+        
+        //Vista inicial 
+        PanelVentas p1 = new PanelVentas();
+        ShowPanel(p1);
     }
     public void getInfoUSer(ArrayList<Object> resultado){
         UserData saveUserData = new UserData();
@@ -32,6 +44,14 @@ public class Sistema extends javax.swing.JFrame {
         saveUserData.setUsername((String) resultado.get(3));
         String name = saveUserData.getName();
         txtWelcomeUser.setText("Bienvenido "+ name);
+    }
+    private void ShowPanel(JPanel p){
+        p.setSize(770,490);
+        p.setLocation(0,0);
+        content.removeAll();
+        content.add(p, BorderLayout.CENTER);
+        content.revalidate();
+        content.repaint();
     }
     
 
@@ -50,19 +70,19 @@ public class Sistema extends javax.swing.JFrame {
         jPanel1 = new javax.swing.JPanel();
         buttonVentas = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
-        jPanel7 = new javax.swing.JPanel();
+        iconVentas = new javax.swing.JLabel();
+        buttonInvetario = new javax.swing.JPanel();
         jLabel3 = new javax.swing.JLabel();
         jPanel9 = new javax.swing.JPanel();
         jLabel5 = new javax.swing.JLabel();
         jPanel2 = new javax.swing.JPanel();
         txtWelcomeUser = new javax.swing.JLabel();
         txtLogo = new javax.swing.JLabel();
-        jPanel6 = new javax.swing.JPanel();
+        content = new javax.swing.JPanel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setLocationByPlatform(true);
         setUndecorated(true);
-        setPreferredSize(new java.awt.Dimension(1000, 660));
         setResizable(false);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
@@ -137,10 +157,17 @@ public class Sistema extends javax.swing.JFrame {
 
         jPanel1.setBackground(new java.awt.Color(255, 184, 112));
 
-        buttonVentas.setBackground(new java.awt.Color(255, 245, 214));
+        buttonVentas.setBackground(new java.awt.Color(217, 97, 83));
         buttonVentas.setPreferredSize(new java.awt.Dimension(100, 50));
+        buttonVentas.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                buttonVentasMouseClicked(evt);
+            }
+        });
 
+        jLabel1.setBackground(new java.awt.Color(255, 255, 255));
         jLabel1.setFont(new java.awt.Font("Lato", 0, 13)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(255, 255, 255));
         jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel1.setText("VENTAS");
 
@@ -149,46 +176,63 @@ public class Sistema extends javax.swing.JFrame {
         buttonVentasLayout.setHorizontalGroup(
             buttonVentasLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(buttonVentasLayout.createSequentialGroup()
-                .addGap(24, 24, 24)
+                .addGap(12, 12, 12)
+                .addComponent(iconVentas, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 56, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGap(0, 0, Short.MAX_VALUE))
         );
         buttonVentasLayout.setVerticalGroup(
             buttonVentasLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(buttonVentasLayout.createSequentialGroup()
-                .addGap(16, 16, 16)
-                .addComponent(jLabel1)
-                .addContainerGap(17, Short.MAX_VALUE))
+                .addGap(11, 11, 11)
+                .addGroup(buttonVentasLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(iconVentas, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel1))
+                .addContainerGap(14, Short.MAX_VALUE))
         );
 
-        jPanel7.setBackground(new java.awt.Color(255, 245, 214));
-        jPanel7.setPreferredSize(new java.awt.Dimension(100, 50));
+        buttonInvetario.setBackground(new java.awt.Color(217, 97, 83));
+        buttonInvetario.setPreferredSize(new java.awt.Dimension(100, 50));
+        buttonInvetario.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                buttonInvetarioMouseClicked(evt);
+            }
+        });
 
+        jLabel3.setBackground(new java.awt.Color(255, 255, 255));
         jLabel3.setFont(new java.awt.Font("Lato", 0, 13)); // NOI18N
+        jLabel3.setForeground(new java.awt.Color(255, 255, 255));
         jLabel3.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel3.setText("INVENTARIO");
 
-        javax.swing.GroupLayout jPanel7Layout = new javax.swing.GroupLayout(jPanel7);
-        jPanel7.setLayout(jPanel7Layout);
-        jPanel7Layout.setHorizontalGroup(
-            jPanel7Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel7Layout.createSequentialGroup()
-                .addGap(24, 24, 24)
+        javax.swing.GroupLayout buttonInvetarioLayout = new javax.swing.GroupLayout(buttonInvetario);
+        buttonInvetario.setLayout(buttonInvetarioLayout);
+        buttonInvetarioLayout.setHorizontalGroup(
+            buttonInvetarioLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(buttonInvetarioLayout.createSequentialGroup()
+                .addGap(41, 41, 41)
                 .addComponent(jLabel3)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
-        jPanel7Layout.setVerticalGroup(
-            jPanel7Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel7Layout.createSequentialGroup()
-                .addGap(16, 16, 16)
+        buttonInvetarioLayout.setVerticalGroup(
+            buttonInvetarioLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(buttonInvetarioLayout.createSequentialGroup()
+                .addGap(17, 17, 17)
                 .addComponent(jLabel3)
-                .addContainerGap(17, Short.MAX_VALUE))
+                .addContainerGap(16, Short.MAX_VALUE))
         );
 
-        jPanel9.setBackground(new java.awt.Color(255, 245, 214));
+        jPanel9.setBackground(new java.awt.Color(217, 97, 83));
         jPanel9.setPreferredSize(new java.awt.Dimension(100, 50));
+        jPanel9.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jPanel9MouseClicked(evt);
+            }
+        });
 
         jLabel5.setFont(new java.awt.Font("Lato", 0, 13)); // NOI18N
+        jLabel5.setForeground(new java.awt.Color(255, 255, 255));
         jLabel5.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel5.setText("CONFIGURACION");
 
@@ -197,16 +241,16 @@ public class Sistema extends javax.swing.JFrame {
         jPanel9Layout.setHorizontalGroup(
             jPanel9Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel9Layout.createSequentialGroup()
-                .addGap(24, 24, 24)
+                .addGap(38, 38, 38)
                 .addComponent(jLabel5)
-                .addContainerGap(86, Short.MAX_VALUE))
+                .addContainerGap(72, Short.MAX_VALUE))
         );
         jPanel9Layout.setVerticalGroup(
             jPanel9Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel9Layout.createSequentialGroup()
-                .addGap(16, 16, 16)
+                .addGap(14, 14, 14)
                 .addComponent(jLabel5)
-                .addContainerGap(17, Short.MAX_VALUE))
+                .addContainerGap(19, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
@@ -214,7 +258,7 @@ public class Sistema extends javax.swing.JFrame {
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addComponent(buttonVentas, javax.swing.GroupLayout.DEFAULT_SIZE, 220, Short.MAX_VALUE)
-            .addComponent(jPanel7, javax.swing.GroupLayout.DEFAULT_SIZE, 220, Short.MAX_VALUE)
+            .addComponent(buttonInvetario, javax.swing.GroupLayout.DEFAULT_SIZE, 220, Short.MAX_VALUE)
             .addComponent(jPanel9, javax.swing.GroupLayout.DEFAULT_SIZE, 220, Short.MAX_VALUE)
         );
         jPanel1Layout.setVerticalGroup(
@@ -223,7 +267,7 @@ public class Sistema extends javax.swing.JFrame {
                 .addGap(182, 182, 182)
                 .addComponent(buttonVentas, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jPanel7, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(buttonInvetario, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jPanel9, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(316, Short.MAX_VALUE))
@@ -231,8 +275,9 @@ public class Sistema extends javax.swing.JFrame {
 
         getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 220, 660));
 
-        jPanel2.setBackground(new java.awt.Color(217, 97, 83));
+        jPanel2.setBackground(new java.awt.Color(255, 245, 214));
 
+        txtWelcomeUser.setBackground(new java.awt.Color(0, 0, 0));
         txtWelcomeUser.setFont(new java.awt.Font("Lato", 0, 18)); // NOI18N
         txtWelcomeUser.setForeground(new java.awt.Color(255, 255, 255));
         txtWelcomeUser.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
@@ -266,18 +311,18 @@ public class Sistema extends javax.swing.JFrame {
 
         getContentPane().add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(220, 0, 780, 110));
 
-        javax.swing.GroupLayout jPanel6Layout = new javax.swing.GroupLayout(jPanel6);
-        jPanel6.setLayout(jPanel6Layout);
-        jPanel6Layout.setHorizontalGroup(
-            jPanel6Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+        javax.swing.GroupLayout contentLayout = new javax.swing.GroupLayout(content);
+        content.setLayout(contentLayout);
+        contentLayout.setHorizontalGroup(
+            contentLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGap(0, 770, Short.MAX_VALUE)
         );
-        jPanel6Layout.setVerticalGroup(
-            jPanel6Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+        contentLayout.setVerticalGroup(
+            contentLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGap(0, 490, Short.MAX_VALUE)
         );
 
-        getContentPane().add(jPanel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(220, 110, 770, 490));
+        getContentPane().add(content, new org.netbeans.lib.awtextra.AbsoluteConstraints(220, 110, 770, 490));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -304,6 +349,21 @@ public class Sistema extends javax.swing.JFrame {
     private void buttoCloseMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_buttoCloseMouseExited
         buttoClose.setBackground(new Color (242,242,242));
     }//GEN-LAST:event_buttoCloseMouseExited
+
+    private void buttonInvetarioMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_buttonInvetarioMouseClicked
+        PanelInventarios p1 = new PanelInventarios();
+        ShowPanel(p1);
+    }//GEN-LAST:event_buttonInvetarioMouseClicked
+
+    private void buttonVentasMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_buttonVentasMouseClicked
+        PanelVentas p1 = new PanelVentas();
+        ShowPanel(p1);
+    }//GEN-LAST:event_buttonVentasMouseClicked
+
+    private void jPanel9MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jPanel9MouseClicked
+        PanelConfiguracion p1 = new PanelConfiguracion();
+        ShowPanel(p1);
+    }//GEN-LAST:event_jPanel9MouseClicked
 
     /**
      * @param args the command line arguments
@@ -344,7 +404,10 @@ public class Sistema extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel buttoClose;
+    private javax.swing.JPanel buttonInvetario;
     private javax.swing.JPanel buttonVentas;
+    private javax.swing.JPanel content;
+    private javax.swing.JLabel iconVentas;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
@@ -352,8 +415,6 @@ public class Sistema extends javax.swing.JFrame {
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JPanel jPanel4;
-    private javax.swing.JPanel jPanel6;
-    private javax.swing.JPanel jPanel7;
     private javax.swing.JPanel jPanel9;
     private javax.swing.JLabel txtLogo;
     private javax.swing.JLabel txtWelcomeUser;
